44dd912571489e61344ca82502d95de6e92f5641
jersonmr <jersonmr@gmail.com>
2025-03-26 16:04:45 -0400

    Add translatable fields to Experience model

--- app/Models/Experience.php
+++ app/Models/Experience.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Trait\Models\HasTranslations;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Experience extends Model
 {
+    use HasTranslations;
     protected $fillable = [
         'user_id',
         'position',
@@ -18,17 +20,25 @@ class Experience extends Model
         'technologies',
         'location',
     ];
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
+
+    protected array $translatable = [
+        'position',
+        'description',
+    ];
 
     protected function casts(): array
     {
         return [
+            'position' => 'json',
+            'description' => 'json',
             'start_date' => 'date',
             'end_date' => 'date',
             'is_freelance' => 'boolean',
         ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
